7f21aab52e953e9dd9774dc65becfc28dea98487
Steven Aizaga <sxaizaga@espe.edu.ec>
2020-08-19 01:18:14 -0500

    feat: Added the VRP class

---
+++ model/VRP.js
@@ -0,0 +1,11 @@
+class VRP {
+    constructor(code, number) {
+        this.number = number
+        this.code = code
+    }
+
+    printVRP() {
+        console.log("Code: " + this.code)
+        console.log("Number: " + this.number)
+    }
+}
